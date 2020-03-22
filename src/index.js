3ef4037f2f79990aaef0ebcbe6ddea5eab87d740
Tein Pirs <tein.pirs@mail.ru>
2020-03-22 13:15:51 +0300

    feat: index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let elementBrackets = bracketsConfig.map( bracket => bracket.join(''));
+    
+    for (let i = 0; i<elementBrackets.length; i++){
+        if (str.includes(elementBrackets[i])){
+            str = str.replace(elementBrackets[i], '');
+            i = -1;
+        } 
+        
+    }
+
+    if (str) {
+        return false;
+    } else {
+        return true;
+    }
 }
